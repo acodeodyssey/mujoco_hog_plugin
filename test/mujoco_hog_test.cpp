9b7647b4ca2b5299503fcbedf008e23926b46cee
Julian Leichert <mylovelyacc@protonmail.com>
2023-06-28 15:31:32 +0200

    feat: added tests for services

--- test/mujoco_hog_test.cpp
+++ test/mujoco_hog_test.cpp
@@ -36,13 +36,8 @@
 
 #include <gtest/gtest.h>
 
-// Ignore static variables unused in this compilation unit
-// TODO(dleins): Remove this after object oriented refactoring
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable"
 #include <mujoco_ros/mujoco_sim.h>
-
-#pragma GCC diagnostic pop
+#include <mujoco_ros/mujoco_env.h>
 
 #include <mujoco_ros/common_types.h>
 #include <mujoco_ros_msgs/SetGeomPosition.h>
@@ -80,16 +75,19 @@ protected:
 	virtual void SetUp()
 	{
 		nh.reset(new ros::NodeHandle("~"));
-		// nh->setParam("unpause", true);
+		nh->setParam("unpause", true);
 		nh->setParam("no_x", true);
-		// nh->setParam("use_sim_time", true);
+		nh->setParam("use_sim_time", true);
 
 		std::string xml_path = ros::package::getPath("mujoco_hog") + "/assets/hog_world.xml";
+		ROS_INFO_STREAM(xml_path);
 
-		mj_thread = std::unique_ptr<std::thread>(new std::thread(MujocoSim::init, xml_path, "example_hash"));
+		mj_thread = std::unique_ptr<std::thread>(new std::thread(MujocoSim::init, xml_path, ""));
+
 		while (MujocoSim::detail::settings_.loadrequest.load() == 0) { // wait for request to be made
 			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		}
+
 		while (MujocoSim::detail::settings_.loadrequest.load() > 0) { // wait for model to be loaded
 			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		}
@@ -108,7 +106,7 @@ protected:
 
 TEST_F(MujocoRosBaseFixture, set_solver_parameters)
 {
-	std::vector<MujocoSim::MujocoPluginPtr> plugins;
+	std::vector<MujocoSim::MujocoPluginPtr> plugins = env->getPlugins();
 
 	for (auto p : plugins) {
 		hogPlugin = dynamic_cast<mujoco_ros_hog::MujocoHogPlugin *>(p.get());
@@ -119,9 +117,7 @@ TEST_F(MujocoRosBaseFixture, set_solver_parameters)
 			return;
 		}
 	}
-	nh->setParam("unpause", false);
 	mujoco_ros_msgs::SetSolverParameters srv;
-	ROS_INFO_STREAM("I'm running the first test now");
 	// correct parameters
 	srv.request.solverParameters.dampratio = 0.01;
 	srv.request.solverParameters.timeconst = 1;
@@ -134,19 +130,83 @@ TEST_F(MujocoRosBaseFixture, set_solver_parameters)
 	srv.request.solverParameters.name      = "hogBox";
 	srv.request.admin_hash                 = "example_hash";
 	hogPlugin->setSolverParametersCB(srv.request, srv.response);
+	EXPECT_TRUE(srv.response.success) << "Setting correct solver parameters failed";
+
+	srv.request.solverParameters.dmax = 0;
+	hogPlugin->setSolverParametersCB(srv.request, srv.response);
+	EXPECT_FALSE(srv.response.success) << "Wrong response for wrong parameters";
+	srv.request.solverParameters.name = "wrongboxname";
+	hogPlugin->setSolverParametersCB(srv.request, srv.response);
 	// wrong body name
-	// mujoco_ros_hogMujocoHogPlugin::setSolverParametersCB(srv.request,srv.response);
-
-	// wrong env_id
+	EXPECT_FALSE(srv.response.success) << "Wrong response for wrong body name";
 }
 
-TEST_F(MujocoRosBaseFixture, set_eq_active)
+TEST_F(MujocoRosBaseFixture, set_weld_constraints)
 {
-	nh->setParam("unpause", false);
-	std_srvs::SetBool srv;
-	srv.request.data          = true;
-	ros::ServiceClient client = nh->serviceClient<std_srvs::SetBool>("mujoco_ros_hog/set_eq_active");
-	EXPECT_TRUE(client.call(srv)) << "Service call failed!";
+	std::vector<MujocoSim::MujocoPluginPtr> plugins = env->getPlugins();
+
+	for (auto p : plugins) {
+		hogPlugin = dynamic_cast<mujoco_ros_hog::MujocoHogPlugin *>(p.get());
+		if (hogPlugin != nullptr) {
+			ROS_INFO_STREAM("Plugin loaded");
+			break;
+		} else {
+			return;
+		}
+	}
+	mujoco_ros_msgs::SetWeldConstraintParameters srv;
+
+	// empty parameters
+	srv.request.admin_hash      = "";
+	srv.request.set_solimp      = false;
+	srv.request.set_solref      = false;
+	srv.request.set_torquescale = false;
+	hogPlugin->setWeldConstraintParametersCB(srv.request, srv.response);
+	EXPECT_FALSE(srv.response.success) << "Empty parameters resulted in wrong response";
+	srv.request.set_solimp                            = true;
+	srv.request.set_solref                            = true;
+	srv.request.set_torquescale                       = true;
+	srv.request.parameters.torquescale                = 0;
+	srv.request.parameters.active                     = true;
+	srv.request.parameters.solverParameters.dampratio = 1;
+	hogPlugin->setWeldConstraintParametersCB(srv.request, srv.response);
+	EXPECT_TRUE(srv.response.success) << "Wr parameters resulted in wrong response";
 }
 
+TEST_F(MujocoRosBaseFixture, set_geom_position)
+{
+	std::vector<MujocoSim::MujocoPluginPtr> plugins = env->getPlugins();
+
+	for (auto p : plugins) {
+		hogPlugin = dynamic_cast<mujoco_ros_hog::MujocoHogPlugin *>(p.get());
+		if (hogPlugin != nullptr) {
+			ROS_INFO_STREAM("Plugin loaded");
+			break;
+		} else {
+			return;
+		}
+	}
+	mujoco_ros_msgs::SetGeomPosition srv;
+
+	// empty parameters
+	srv.request.admin_hash   = "";
+	srv.request.set_position = false;
+	hogPlugin->setGeomPositionCB(srv.request, srv.response);
+	EXPECT_TRUE(srv.response.success) << "Parameters resulted in wrong response";
+	srv.request.position.env_id             = 0;
+	srv.request.position.name               = "wrongboxname";
+	srv.request.position.pose.orientation.w = 1.0;
+	srv.request.position.pose.orientation.x = 0.0;
+	srv.request.position.pose.orientation.y = 0.0;
+	srv.request.position.pose.orientation.z = 0.0;
+	srv.request.position.pose.position.x    = 0.0;
+	srv.request.position.pose.position.y    = 0.0;
+	srv.request.position.pose.position.z    = 0.0;
+
+	hogPlugin->setGeomPositionCB(srv.request, srv.response);
+	EXPECT_FALSE(srv.response.success) << "Wrong bodyname parameter resulted in wrong response";
+	srv.request.position.name = "hogBox";
+	hogPlugin->setGeomPositionCB(srv.request, srv.response);
+	EXPECT_TRUE(srv.response.success) << "Wr parameters resulted in wrong response";
+}
 } // namespace unit_testing
